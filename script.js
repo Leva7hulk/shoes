ea8cfc9b9e30b5e1bf1adb71afff604d81e10c52
Leva7hulk <gzirishvililevan7@gmail.com>
2025-02-28 20:18:14 +0400

    script.js

--- script.js
+++ script.js
@@ -13,50 +13,161 @@ const firebaseConfig = {
   const app = firebase.initializeApp(firebaseConfig);
   const database = firebase.database();
   
-  const shoes = [
+const shoes = [
+    // Original 3 shoes
     {
-      id: 1,
-      name: "ლურჯი სპორტული კედები",
-      image: "https://i.imgur.com/pdp6hvV.jpeg",
-      sizes: [40, 41, 42, 43, 44]
-    },
-    {
-      id: 2,
-      name: "წითელი სარბენი ფეხსაცმელი",
-      image: "https://i.imgur.com/FqVmEFi.jpeg",
-      sizes: [40, 41, 42, 43, 44]
+        id: 1,
+        name: "ლურჯი სპორტული კედები",
+        image: "https://i.imgur.com/pdp6hvV.jpeg",
+        sizes: [40, 41, 42, 43, 44]
     },
     {
-      id: 3,
-      name: "შავი ოფიციალური ფეხსაცმელი",
-      image: "https://i.imgur.com/gnYMv05.jpeg",
-      sizes: [40, 41, 42, 43, 44]
+        id: 2,
+        name: "წითელი სარბენი ფეხსაცმელი",
+        image: "shoe-image-2.jpg",
+        sizes: [39, 40, 41, 42, 45]
     },
-      {
-      id: 4,
-      name: "შავი ოფიციალური ფეხსაცმელი1",
-      image: "https://i.imgur.com/gnYMv05.jpeg",
-      sizes: [40, 41, 42, 43, 44]
+    {
+        id: 3,
+        name: "შავი ოფიციალური ფეხსაცმელი",
+        image: "shoe-image-3.jpg", 
+        sizes: [41, 42, 43, 44, 45]
     },
-      {
-      id: 5,
-      name: "შავი ოფიციალური ფეხსაცმელი2",
-      image: "https://i.imgur.com/gnYMv05.jpeg",
-      sizes: [40, 41, 42, 43, 44]
+
+    // New shoes 4-25
+    {
+        id: 4,
+        name: "მწვანე კაზუალური ფეხსაცმელი",
+        image: "https://picsum.photos/200/300?random=1",
+        sizes: [38, 39, 40, 41, 42]
     },
-      {
-      id: 6,
-      name: "შავი ოფიციალური ფეხსაცმელი3",
-      image: "https://i.imgur.com/gnYMv05.jpeg",
-      sizes: [40, 41, 42, 43, 44]
+    {
+        id: 5,
+        name: "ვარდისფერი სპორტული სნიკერსი",
+        image: "https://picsum.photos/200/300?random=2",
+        sizes: [36, 37, 38, 39, 40]
     },
-      {
-      id: 7,
-      name: "შავი ოფიციალური ფეხსაცმელი4",
-      image: "https://i.imgur.com/gnYMv05.jpeg",
-      sizes: [40, 41, 42, 43, 44]
+    {
+        id: 6,
+        name: "ნაცრისფერი უნისექსი ბოტასი",
+        image: "https://picsum.photos/200/300?random=3",
+        sizes: [39, 40, 41, 42, 43]
+    },
+    {
+        id: 7,
+        name: "ოქროსფერი ელეგანტური სანდლები",
+        image: "https://picsum.photos/200/300?random=4",
+        sizes: [37, 38, 39, 40, 41]
+    },
+    {
+        id: 8,
+        name: "თეთრი ბასკეტბოლის ფეხსაცმელი",
+        image: "https://picsum.photos/200/300?random=5",
+        sizes: [42, 43, 44, 45, 46]
+    },
+    {
+        id: 9,
+        name: "ყავისფერი ტურისტული ბოტინი",
+        image: "https://picsum.photos/200/300?random=6",
+        sizes: [40, 41, 42, 43, 44]
+    },
+    {
+        id: 10,
+        name: "მუქი ლურჯი სკეიტბორდის ფეხსაცმელი",
+        image: "https://picsum.photos/200/300?random=7",
+        sizes: [39, 40, 41, 42, 43]
+    },
+    {
+        id: 11,
+        name: "ბროწეულისფერი რანერები",
+        image: "https://picsum.photos/200/300?random=8",
+        sizes: [36, 37, 38, 39, 40]
+    },
+    {
+        id: 12,
+        name: "მოლურჯო ზამთრის ბოტასი",
+        image: "https://picsum.photos/200/300?random=9",
+        sizes: [41, 42, 43, 44, 45]
+    },
+    {
+        id: 13,
+        name: "შოკოლადისფერი დერიბი",
+        image: "https://picsum.photos/200/300?random=10",
+        sizes: [38, 39, 40, 41, 42]
+    },
+    {
+        id: 14,
+        name: "ვერცხლისფერი ფიტნესის ფეხსაცმელი",
+        image: "https://picsum.photos/200/300?random=11",
+        sizes: [39, 40, 41, 42, 43]
+    },
+    {
+        id: 15,
+        name: "ლილასფერი აერობიკის ფეხსაცმელი",
+        image: "https://picsum.photos/200/300?random=12",
+        sizes: [37, 38, 39, 40, 41]
+    },
+    {
+        id: 16,
+        name: "კამუფფერი სამხედრო სტილის ბოტინი",
+        image: "https://picsum.photos/200/300?random=13",
+        sizes: [42, 43, 44, 45, 46]
+    },
+    {
+        id: 17,
+        name: "ნარინჯისფერი მთათამაშხა ფეხსაცმელი",
+        image: "https://picsum.photos/200/300?random=14",
+        sizes: [40, 41, 42, 43, 44]
+    },
+    {
+        id: 18,
+        name: "მოვარდისფრო ფუტბოლის ფეხსაცმელი",
+        image: "https://picsum.photos/200/300?random=15",
+        sizes: [39, 40, 41, 42, 43]
+    },
+    {
+        id: 19,
+        name: "ზეთისხილისფერი კლასიკური ლოფერები",
+        image: "https://picsum.photos/200/300?random=16",
+        sizes: [36, 37, 38, 39, 40]
+    },
+    {
+        id: 20,
+        name: "მუქი მწვანე ტრეკინგის ფეხსაცმელი",
+        image: "https://picsum.photos/200/300?random=17",
+        sizes: [41, 42, 43, 44, 45]
+    },
+    {
+        id: 21,
+        name: "ღია ლურჯი ფიტნესის სნიკერსი",
+        image: "https://picsum.photos/200/300?random=18",
+        sizes: [38, 39, 40, 41, 42]
+    },
+    {
+        id: 22,
+        name: "შავი თეთრი პატერნიანი კედები",
+        image: "https://picsum.photos/200/300?random=19",
+        sizes: [39, 40, 41, 42, 43]
+    },
+    {
+        id: 23,
+        name: "ოქროსფერი საღამოს ფეხსაცმელი",
+        image: "https://picsum.photos/200/300?random=20",
+        sizes: [37, 38, 39, 40, 41]
+    },
+    {
+        id: 24,
+        name: "ტყვიისფერი საველე ფეხსაცმელი",
+        image: "https://picsum.photos/200/300?random=21",
+        sizes: [42, 43, 44, 45, 46]
+    },
+    {
+        id: 25,
+        name: "მულტიფერი ბავშვების ფეხსაცმელი",
+        image: "https://picsum.photos/200/300?random=22",
+        sizes: [28, 29, 30, 31, 32]
     }
-  ];
+];
   
   function createShoeElement(shoe) {
     const productContainer = document.createElement('div');
